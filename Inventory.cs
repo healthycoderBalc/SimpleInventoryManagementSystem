fc94a43dac47695019f3a278b4f29c402e8706e9
Maria Romero <mmrbalcazar@gmail.com>
2024-06-22 21:06:33 -0300

    Update CRUD operations to use ms sql database

--- Inventory.cs
+++ Inventory.cs
@@ -3,37 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace SimpleInventoryManagementSystem
 {
     internal static class Inventory
     {
-        private static List<Product> products = new List<Product>();
+        private static string connectionString = ConfigurationManager.ConnectionStrings["InventoryManagementSystemDB"].ConnectionString;
 
         public static void AddProduct()
         {
             string productName = Utilities.RequestProductName();
             double productPrice = Utilities.RequestProductPriceOrQuantity("price");
-            double productQuantity = Utilities.RequestProductPriceOrQuantity("quantity");
-            Product p = new Product(productName, productPrice, productQuantity);
-            products.Add(p);
-            Console.WriteLine($"This is the product you added: ");
-            Console.WriteLine(p.ToString());
+            int productQuantity = (int)Utilities.RequestProductPriceOrQuantity("quantity");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "INSERT INTO Products (Name, Price, Quantity) VALUES (@Name, @Price, @Quantity)";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Name", productName);
+                    command.Parameters.AddWithValue("@Price", productPrice);
+                    command.Parameters.AddWithValue("@Quantity", productQuantity);
+                    command.ExecuteNonQuery();
+                }
+
+            }
+
+            Console.WriteLine("Product added to the database.");
         }
 
         public static void ViewAllProducts()
         {
-            Console.WriteLine("|--------------------------------------------|");
-            Console.WriteLine("|   Product    |    $Price    |   Quantity   |");
-            Console.WriteLine("|--------------------------------------------|");
-            for (int i = 0; i < products.Count; i++)
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                string name = Utilities.CompletingColumnSize(products[i].Name, 14);
-                string price = Utilities.CompletingColumnSize(products[i].Price.ToString(), 14); ;
-                string quantity = Utilities.CompletingColumnSize(products[i].Quantity.ToString(), 14);
-                Console.WriteLine($"|{name}|{price}|{quantity}|");
+                connection.Open();
+                string query = "SELECT * FROM Products";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        Console.WriteLine("|--------------------------------------------|");
+                        Console.WriteLine("|   Product    |    $Price    |   Quantity   |");
+                        Console.WriteLine("|--------------------------------------------|");
+                        while (reader.Read())
+                        {
+                            string name = Utilities.CompletingColumnSize(reader["Name"].ToString(), 14);
+                            string price = Utilities.CompletingColumnSize(reader["Price"].ToString(), 14); ;
+                            string quantity = Utilities.CompletingColumnSize(reader["Quantity"].ToString(), 14);
+                            Console.WriteLine($"|{name}|{price}|{quantity}|");
+
+                        }
+                        Console.WriteLine("|--------------------------------------------|");
+
+                    }
+                }
             }
-            Console.WriteLine("|--------------------------------------------|");
         }
 
         public static void ManageProduct(string text, string selection)
@@ -44,26 +71,44 @@ namespace SimpleInventoryManagementSystem
             Console.WriteLine("********************************");
             string productName = Utilities.RequestProductName();
             bool found = false;
-            for (int i = 0; i < products.Count; i++)
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                if (products[i].Name == productName)
+                connection.Open();
+                string query = "SELECT * FROM Products WHERE Name = @Name";
+                using (SqlCommand command = new SqlCommand(query, connection))
                 {
-                    found = true;
-                    Console.WriteLine("Product found: ");
-                    Console.WriteLine(products[i].ToString());
-                    switch (selection)
+                    command.Parameters.AddWithValue("@Name", productName);
+                    using (SqlDataReader reader = command.ExecuteReader())
                     {
-                        case "3":
-                            EditProduct(products[i]);
-                            break;
-                        case "4":
-                            DeleteProduct(products[i]);
-                            break;
-                        case "5":
-                            break;
+                        if (reader.Read())
+                        {
+                            found = true;
+                            Product product = new Product(
+                                reader["Name"].ToString(),
+                                Convert.ToDouble(reader["Price"]),
+                                Convert.ToInt32(reader["Quantity"])
+                            );
+                            product.ProductId = Convert.ToInt32(reader["ProductID"]);
+
+                            Console.WriteLine("Product found: ");
+                            Console.WriteLine(product.ToString());
+
+                            switch (selection)
+                            {
+                                case "3":
+                                    EditProduct(product);
+                                    break;
+                                case "4":
+                                    DeleteProduct(product);
+                                    break;
+                                case "5":
+                                    break;
+                            }
+                        }
                     }
-                    break;
                 }
+
             }
             if (!found)
             {
@@ -88,25 +133,53 @@ namespace SimpleInventoryManagementSystem
 
         private static void EditProductAttribute(Product p, string selection)
         {
-            switch (selection)
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                case "1":
-                    string productName = Utilities.RequestProductName();
-                    p.Name = productName;
-                    break;
-                case "2":
-                    double productPrice = Utilities.RequestProductPriceOrQuantity("price");
-                    p.Price = productPrice;
-                    break;
-                case "3":
-                    double productQuantity = Utilities.RequestProductPriceOrQuantity("quantity");
-                    p.Quantity = productQuantity;
-                    break;
-                case "0":
-                    break;
-                default:
-                    Console.WriteLine("Yoy have not selected a valid option, please try again: ");
-                    break;
+                connection.Open();
+                string query = "";
+
+                switch (selection)
+                {
+                    case "1":
+                        string productName = Utilities.RequestProductName();
+                        p.Name = productName;
+                        query = "UPDATE Products SET Name = @Name WHERE ProductID = @ProductID";
+                        break;
+                    case "2":
+                        double productPrice = Utilities.RequestProductPriceOrQuantity("price");
+                        p.Price = productPrice;
+                        query = "UPDATE Products SET Price = @Price WHERE ProductID = @ProductID";
+                        break;
+                    case "3":
+                        double productQuantity = Utilities.RequestProductPriceOrQuantity("quantity");
+                        p.Quantity = productQuantity;
+                        query = "UPDATE Products SET Quantity = @Quantity WHERE ProductID = @ProductID";
+                        break;
+                    case "0":
+                        return;
+                    default:
+                        Console.WriteLine("Yoy have not selected a valid option, please try again: ");
+                        return;
+
+                }
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    if (selection == "1")
+                    {
+                        command.Parameters.AddWithValue("@Name", p.Name);
+                    }
+                    else if (selection == "2")
+                    {
+                        command.Parameters.AddWithValue("@Price", p.Price);
+                    }
+                    else if (selection == "3")
+                    {
+                        command.Parameters.AddWithValue("@Quantity", p.Quantity);
+                    }
+                    command.Parameters.AddWithValue("@ProductID", p.ProductId);
+                    command.ExecuteNonQuery();
+                }
 
             }
 
@@ -120,7 +193,16 @@ namespace SimpleInventoryManagementSystem
 
             if (remove == "y")
             {
-                products.Remove(p);
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "DELETE FROM Products WHERE ProductID = @ProductID";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@ProductId", p.ProductId);
+                        command.ExecuteNonQuery();
+                    }
+                }
                 Console.WriteLine("The product has been deleted successfully");
                 Console.WriteLine("This is the updated list of products in the inventory: ");
             }
